8352820a3b278dfcef33c331ce817637dcdef492
Karem Darwesh <karemdarwesh1035@gmail.com>
2025-11-07 20:49:29 +0200

    Add user and habits tables to the database schema

---
+++ src/db/schema.ts
@@ -0,0 +1,28 @@
+import {
+  pgTable,
+  uuid,
+  varchar,
+  text,
+  timestamp,
+  boolean,
+  integer,
+} from 'drizzle-orm/pg-core'
+
+import { relations } from 'drizzle-orm'
+
+export const users = pgTable('users', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  email: varchar('email', { length: 255 }).notNull().unique(),
+  username: varchar('username', { length: 50 }).notNull().unique(),
+  password: varchar('password', {
+    length: 255,
+  }).notNull(),
+  firstName: varchar('first_name', { length: 50 }),
+  lastName: varchar('last_name', { length: 50 }),
+  createAt: timestamp('create_at').defaultNow().notNull,
+  updatedAt: timestamp('updated_at').defaultNow().notNull,
+})
+
+export const habits = pgTable('habits', {
+  id: uuid('id').primaryKey().defaultRandom(),
+})
