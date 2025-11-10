b6d225de546521ed50e0f3090cc5572de68074ac
Karem Darwesh <karemdarwesh1035@gmail.com>
2025-11-10 05:08:39 +0200

    Update schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -9,6 +9,7 @@ import {
 } from 'drizzle-orm/pg-core'
 
 import { relations } from 'drizzle-orm'
+import { id } from 'zod/locales'
 
 export const users = pgTable('users', {
   id: uuid('id').primaryKey().defaultRandom(),
@@ -19,10 +20,101 @@ export const users = pgTable('users', {
   }).notNull(),
   firstName: varchar('first_name', { length: 50 }),
   lastName: varchar('last_name', { length: 50 }),
-  createAt: timestamp('create_at').defaultNow().notNull,
-  updatedAt: timestamp('updated_at').defaultNow().notNull,
+  createAt: timestamp('create_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
 })
+
+import { createInsertSchema, createSelectSchema } from 'drizzle-zod'
 
 export const habits = pgTable('habits', {
   id: uuid('id').primaryKey().defaultRandom(),
+  userId: uuid('user_id')
+    .references(() => users.id, { onDelete: 'cascade' })
+    .notNull(),
+  name: varchar('name', { length: 50 }).notNull(),
+  description: text('description'),
+  frequency: varchar('frequency', { length: 20 }).notNull(),
+  targetCount: integer('target_count').default(1),
+  isActive: boolean('is_active').default(true),
+  createAt: timestamp('create_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
 })
+
+export const entries = pgTable('entries', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  habitId: uuid('habit_id')
+    .references(() => habits.id, {
+      onDelete: 'cascade',
+    })
+    .notNull(),
+  completionDate: timestamp('completion_date').defaultNow(),
+  note: text('note'),
+  createAt: timestamp('create_at').defaultNow().notNull(),
+})
+
+export const tags = pgTable('tags', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  name: varchar('name', { length: 50 }).notNull().unique(),
+  color: varchar('color', { length: 7 }).default('#6b7280'),
+  createAt: timestamp('create_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
+})
+
+export const habitTags = pgTable('habitTags', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  habitId: uuid('habit_id')
+    .references(() => habits.id, {
+      onDelete: 'cascade',
+    })
+    .notNull(),
+  tagId: uuid('tag_id')
+    .references(() => tags.id, {
+      onDelete: 'cascade',
+    })
+    .notNull(),
+})
+
+export const userRelations = relations(users, ({ many }) => ({
+  habits: many(habits),
+}))
+
+export const habitsRelations = relations(habits, ({ one, many }) => ({
+  user: one(users, {
+    fields: [habits.userId],
+    references: [users.id],
+  }),
+  entries: many(entries),
+  habitTags: many(habitTags),
+}))
+
+export const entriesRelations = relations(entries, ({ one }) => ({
+  habit: one(habits, {
+    fields: [entries.habitId],
+    references: [habits.id],
+  }),
+}))
+
+export const tagsRelations = relations(tags, ({ many }) => ({
+  habitTags: many(habitTags),
+}))
+
+export const habitTagsRelations = relations(habitTags, ({ one }) => ({
+  habit: one(habits, {
+    fields: [habitTags.habitId],
+    references: [habits.id],
+  }),
+
+  tag: one(tags, {
+    fields: [habitTags.tagId],
+    references: [tags.id],
+  }),
+}))
+
+export type User = typeof users.$inferSelect
+export type Habit = typeof habits.$inferSelect
+export type Entry = typeof entries.$inferSelect
+export type Tag = typeof tags.$inferSelect
+export type HabitTag = typeof habitTags.$inferSelect
+
+export const insertUserSchema = createInsertSchema(users)
+export const selectUserSchema = createSelectSchema(users)
