a269e6dcaaadcd57734a7563c549e35de000cdf6
Karem Darwesh <karemdarwesh1035@gmail.com>
2025-11-12 00:33:46 +0200

    Login done

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -3,7 +3,9 @@ import bcrypt from 'bcrypt'
 import { db } from '../db/connection.ts'
 import { users, type NewUser } from '../db/schema.ts'
 import { generateToken } from '../utils/jwt.ts'
-import { hashPassword } from '../utils/passwords.ts'
+import { comparePasswords, hashPassword } from '../utils/passwords.ts'
+import { eq } from 'drizzle-orm'
+
 export const register = async (
   req: Request<any, any, NewUser>,
   res: Response
@@ -41,5 +43,51 @@ export const register = async (
     res.status(500).json({
       error: 'Failed to create user',
     })
+  }
+}
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body
+    const user = await db.query.users.findFirst({
+      where: eq(users.email, email),
+    })
+
+    if (!user) {
+      return res.status(401).json({
+        message: 'Invalid credentials',
+      })
+    }
+
+    const isValidPassword = await comparePasswords(password, user.password)
+
+    if (!isValidPassword) {
+      return res.status(401).json({
+        message: 'Invalid credentials',
+      })
+    }
+
+    const token = await generateToken({
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    })
+
+    return res
+      .json({
+        message: 'Login success',
+        user: {
+          id: user.id,
+          email: user.email,
+          username: user.username,
+          firstName: user.firstName,
+          lastName: user.lastName,
+        },
+        token,
+      })
+      .status(201)
+  } catch (e) {
+    console.error('Login error', e)
+    res.status(500).json({ error: 'Failed to login' })
   }
 }
