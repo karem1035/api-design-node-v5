d57d00158cdc5e9ec97c2534e6c2604cbaf1072b
Karem Darwesh <karemdarwesh1035@gmail.com>
2025-11-11 22:41:00 +0200

    register done

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,12 +1,10 @@
 import { Router } from 'express'
-
+import { register } from '../controllers/authController.ts'
+import { validateBody } from '../middleware/validation.ts'
+import { insertUserSchema } from '../db/schema.ts'
 const router = Router()
 
-router.post('/register', (req, res) => {
-  res.status(201).json({
-    message: 'User registered successfully',
-  })
-})
+router.post('/register', validateBody(insertUserSchema), register)
 
 router.post('/login', (req, res) => {
   res.status(200).json({
---
+++ src/utils/jwt.ts
@@ -0,0 +1,21 @@
+import { SignJWT } from 'jose'
+import { createSecretKey } from 'node:crypto'
+import env from '../../env.ts'
+
+export interface JwtPayload {
+  id: string
+  email: string
+  username: string
+  [key: string]: unknown
+}
+
+export const generateToken = (payload: JwtPayload) => {
+  const secret = env.JWT_SECRET
+  const secretKey = createSecretKey(secret, 'utf-8')
+
+  return new SignJWT(payload)
+    .setProtectedHeader({ alg: 'HS256' })
+    .setIssuedAt()
+    .setExpirationTime(env.JWT_EXPIRES_IN || '7d')
+    .sign(secretKey)
+}
